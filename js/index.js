a7334428a857a00a2face59f802058cd8521128e
kathrynw298 <kathryncwilliams298@gmail.com>
2024-04-15 12:15:22 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,17 +1,13 @@
 console.log("Your index.js file is loaded correctly!");
 
 
-
-
 // Activate scrollspy on your sidebar navigation
 const scrollSpy = new bootstrap.ScrollSpy(document.body, {
   target: '#sidemenu', // ID of your sidebar element
   offset: 200, // Offset from the top to trigger the change
 });
 
-AOS.init({
-  duration: 1200,
-})
+
 
 function myFunction() {
   var x = document.getElementById("myTopnav");
@@ -32,29 +28,3 @@ button.addEventListener('mouseout', () => {
   button.style.backgroundColor = '#FE5D26';
 });
 
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
